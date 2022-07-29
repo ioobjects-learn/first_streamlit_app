633135c28644d4311e0657ff6421d43d23d107b8
ioobjects-learn <110248168+ioobjects-learn@users.noreply.github.com>
2022-07-29 12:41:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title("My Parents New Healthy Dinner")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega3 & Blueberry Oatmeal')
@@ -8,3 +9,6 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('===Build Your Own Fruite Snoothie==========')
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
