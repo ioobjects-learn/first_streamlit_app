43bb58e4d140e7719af6b1bc94052b8224365f92
ioobjects-learn <110248168+ioobjects-learn@users.noreply.github.com>
2022-07-29 12:43:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,6 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('===Build Your Own Fruite Snoothie==========')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index)
 
 streamlit.dataframe(my_fruit_list)
